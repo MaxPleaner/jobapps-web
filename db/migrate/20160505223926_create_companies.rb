8d76f97cd7effa69b161641c749b4fee9c1a4c4f
maxpleaner <maxpleaner@gmail.com>
2016-05-05 15:40:10 -0700

    rails new with company model

---
+++ db/migrate/20160505223926_create_companies.rb
@@ -0,0 +1,15 @@
+class CreateCompanies < ActiveRecord::Migration[5.0]
+  def change
+    create_table :companies do |t|
+      t.string :name
+      t.text :desc
+      t.text :applied
+      t.text :todo
+      t.text :skip
+      t.text :rejected
+      t.string :notlaughing
+
+      t.timestamps
+    end
+  end
+end
