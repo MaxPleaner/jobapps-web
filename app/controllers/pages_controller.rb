c4723de8b006a99e7836b2dd556d8ed19bac6176
maxpleaner <maxpleaner@gmail.com>
2016-05-05 19:14:38 -0700

    skip view and index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,17 @@ class PagesController < ApplicationController
     filter = params[:filter]
     if filter && filter.eql?("only_todos")
       session["only_todos"] = true
+      session["most_recent_skips"] = false
     elsif filter && filter.eql?("not_only_todos")
       session["only_todos"] = false
+      session["most_recent_skips"] = false
+    elsif filter && filter.eql?("most_recent_skips")
+      session["most_recent_skips"] = true
+      @most_recent_skip_count = params[:most_recent_skip_count] || session["most_recent_skip_count"] || 5
+      session["most_recent_skip_count"] = @most_recent_skip_count
+      session["only_todos"] = false
     end
+    @most_recent_skip_count = session["most_recent_skip_count"] || 5
     @percentage_completed = "#{((Company.nonblank.count.to_f / Company.count.to_f) * 100.to_f).round(2)}%"
     @todos_count = Company.todo.count
     @application_count = Company.applied.count
@@ -15,6 +23,8 @@ class PagesController < ApplicationController
     else
       if session["only_todos"]
         @company = Company.todo.limit(1).first
+      elsif session["most_recent_skips"]
+        @most_recent_skips = Company.skipped.last(@most_recent_skip_count)
       else
         @company = Company.blank.limit(1).first
       end
@@ -43,6 +53,8 @@ class PagesController < ApplicationController
       @company.update(notlaughing: params[:update_value])
     when "undo_todo"
       @company.update(todo: nil)
+    when "undo skip"
+      @company.update(skip: nil)
     end
     if params[:next_id]
       redirect_to "/?id=#{params[:next_id]}"
@@ -53,7 +65,7 @@ class PagesController < ApplicationController
 
 
   def params(*args)
-    super(*args).permit(:all, :category, :id, :authenticity_token, :update_key, :update_value, :cmd, :filter, :next_id)
+    super(*args).permit(:all, :category, :id, :authenticity_token, :update_key, :update_value, :cmd, :filter, :next_id, :most_recent_skip_count)
   end
 
 end
