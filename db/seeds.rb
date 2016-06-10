e22790a15c6f01ae00d4f5ddc3ebd7bf88137a16
Max Pleaner <maxpleaner@gmail.com>
2016-06-10 10:56:03 -0700

    better seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
+require 'yaml'
+require('colored')
 
 Dir.glob(Rails.root.join("db/seeds/yml/**/*")).each do |filename|
   objects = YAML.load(File.read(filename))
@@ -24,5 +26,17 @@ Company.select(:category).distinct.pluck(:category).each do |category|
 end
 
 if Company.count.eql?(0)
-  Company.create(name: "foo", desc: "bar", category: Category.create(name: "foo-category", hidden: false).name)
+  # Every company's category attribute should correspond to a db/seeds/<category>.yml file.
+  # Seeds are the only way to import companies into this app.
+  File.open(Rails.root.join("db/seeds/yml/sample_category.yml"), 'w') do |file|
+    company = Company.create(
+      name: "foo",
+      desc: "bar",
+      category: Category.create(name: "sample_category", hidden: false).name)
+    )
+    File.write(YAML.dump(conmpany.attributes))
+    puts "There were no companies in the database".yellow_on_black
+    puts "Created a sample company in db/seeds/yml/sample_category.yml".yellow_on_black
+    puts "------------\|".white_on_black + "Please run the rake db:seed command again.".yellow_on_black + "------------".white_on_black
+  end
 end
